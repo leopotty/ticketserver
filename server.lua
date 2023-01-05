b15a8d763ce0a6152c139b27b2b3763b06845bc3
x <91684842+leopotty@users.noreply.github.com>
2023-01-05 17:39:33 +0100

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,99 @@
+local https = game:GetService("HttpService")
+local Einstellungen = require(script.Parent.Einstellungen)
+local Event = game:GetService("ReplicatedStorage"):WaitForChild("Ticket Events").Senden
+local url = "https://discord.com/api/webhooks/1060590190650994779/EFk73kvkXAgE1GCu7BloYf88lf05cME13IrtN49OAFJFXD0FR8KTT3gwmXQpMUx2dW4O"
+
+
+local function createMessage(url, message : string)
+	local data = {
+		["content"] = message
+	}
+	local finalData = https:JSONEncode(data)
+	local finalUrl = string.gsub(url, "discord.com", "webhook.lewistehminerz.dev")
+	local finalBackupUrl = string.gsub(url, "discord.com", "webhook.newstargeted.com")
+	local good, bad = pcall(function()
+		https:PostAsync(finalUrl, finalData)
+	end)
+	if good then
+		print("Webhook Request Success!")
+	else
+		print("Webhook Request Failed " .. bad .. " Trying backup URL")
+		https:PostAsync(finalBackupUrl, finalData)
+		print("Attempted request with backup URL, if webhook does not send then both URLS are down or you have a bad request.")
+	end
+end
+
+
+local function createEmbed(url, title : string, message :string , fields, image)
+	local data = {
+		['content'] = "",
+		['embeds'] = {{
+			["image"] = {["url"] = image},
+			['title'] = "**"..title.."**",
+			['description'] = message,
+			['type'] = "rich",
+			["color"] = tonumber(Einstellungen.EmbedFarbe),
+			['fields'] = fields
+
+		},
+		},
+	}
+	local finalData = https:JSONEncode(data)
+	local finalUrl = string.gsub(url, "discord.com", "webhook.lewistehminerz.dev")
+	local finalBackupUrl = string.gsub(url, "discord.com", "webhook.newstargeted.com")
+
+	local good, bad = pcall(function()
+		https:PostAsync(finalUrl, finalData)
+	end)
+	if good then
+		print("Webhook Request Success!")
+	else
+		print("Webhook Request Failed " .. bad .. " Trying backup URL")
+		https:PostAsync(finalBackupUrl, finalData)
+		print("Attempted request with backup URL, if webhook does not send then both URLS are down or you have a bad request.")
+
+	end
+end
+
+
+local function createAuthorEmbed(url, authorName : string, iconurl, description : string, fields)
+	local data = {
+		["embeds"] = {{
+			["author"] = {
+				["name"] = authorName,
+				["icon_url"] = iconurl,
+			},
+			["description"] = description,
+			["color"] = tonumber(0xFFFAFA),
+			["fields"] = fields
+
+
+		}},
+	}
+
+	local finalData = https:JSONEncode(data)
+	local finalUrl = string.gsub(url, "discord.com", "webhook.lewistehminerz.dev")
+	local finalBackupUrl = string.gsub(url, "discord.com", "webhook.newstargeted.com")
+
+	local good, bad = pcall(function()
+		https:PostAsync(finalUrl, finalData)
+	end)
+	if good then
+		print("Webhook Request Success!")
+	else
+		print("Webhook Request Failed " .. bad .. " Trying backup URL")
+		https:PostAsync(finalBackupUrl, finalData)
+		print("Attempted request with backup URL, if webhook does not send then both URLS are down or you have a bad request.")
+	end
+end
+
+
+Event.OnServerEvent:Connect(function(player, Art, Spieler, Grund)
+
+	if Art == "SENDEN" then
+
+		createEmbed(url, "TICKET", "**[SPIELER]** :  "..player.Name.."\n**[GEMELDET]** :  "..Spieler.."\n**[GRUND]** :  "..Grund)
+
+	end
+
+end)
