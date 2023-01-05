6dbb5440ecb60793c92d4679d9b147d346ca309d
x <91684842+leopotty@users.noreply.github.com>
2023-01-05 17:37:52 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-local webhookService = {}
-
 local https = game:GetService("HttpService")
 local Einstellungen = require(script.Parent.Einstellungen)
+local Event = game:GetService("ReplicatedStorage"):WaitForChild("Ticket Events").Senden
+local url = "https://discord.com/api/webhooks/1060590190650994779/EFk73kvkXAgE1GCu7BloYf88lf05cME13IrtN49OAFJFXD0FR8KTT3gwmXQpMUx2dW4O"
 
 
-function webhookService:createMessage(url, message : string)
+local function createMessage(url, message : string)
 	local data = {
 		["content"] = message
 	}
@@ -24,7 +24,7 @@ function webhookService:createMessage(url, message : string)
 end
 
 
-function webhookService:createEmbed(url, title : string, message :string , fields, image)
+local function createEmbed(url, title : string, message :string , fields, image)
 	local data = {
 		['content'] = "",
 		['embeds'] = {{
@@ -41,7 +41,7 @@ function webhookService:createEmbed(url, title : string, message :string , field
 	local finalData = https:JSONEncode(data)
 	local finalUrl = string.gsub(url, "discord.com", "webhook.lewistehminerz.dev")
 	local finalBackupUrl = string.gsub(url, "discord.com", "webhook.newstargeted.com")
-	
+
 	local good, bad = pcall(function()
 		https:PostAsync(finalUrl, finalData)
 	end)
@@ -51,12 +51,12 @@ function webhookService:createEmbed(url, title : string, message :string , field
 		print("Webhook Request Failed " .. bad .. " Trying backup URL")
 		https:PostAsync(finalBackupUrl, finalData)
 		print("Attempted request with backup URL, if webhook does not send then both URLS are down or you have a bad request.")
-		
+
 	end
 end
 
 
-function webhookService:createAuthorEmbed(url, authorName : string, iconurl, description : string, fields)
+local function createAuthorEmbed(url, authorName : string, iconurl, description : string, fields)
 	local data = {
 		["embeds"] = {{
 			["author"] = {
@@ -66,15 +66,15 @@ function webhookService:createAuthorEmbed(url, authorName : string, iconurl, des
 			["description"] = description,
 			["color"] = tonumber(0xFFFAFA),
 			["fields"] = fields
-			
-			
+
+
 		}},
 	}
-	
+
 	local finalData = https:JSONEncode(data)
 	local finalUrl = string.gsub(url, "discord.com", "webhook.lewistehminerz.dev")
 	local finalBackupUrl = string.gsub(url, "discord.com", "webhook.newstargeted.com")
-	
+
 	local good, bad = pcall(function()
 		https:PostAsync(finalUrl, finalData)
 	end)
@@ -88,4 +88,12 @@ function webhookService:createAuthorEmbed(url, authorName : string, iconurl, des
 end
 
 
-return webhookService
+Event.OnServerEvent:Connect(function(player, Art, Spieler, Grund)
+
+	if Art == "SENDEN" then
+
+		createEmbed(url, "TICKET", "**[SPIELER]** :  "..player.Name.."\n**[GEMELDET]** :  "..Spieler.."\n**[GRUND]** :  "..Grund)
+
+	end
+
+end)
